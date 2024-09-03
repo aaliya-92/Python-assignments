5e28289cd372bb1c2d1d022ef5a561b75608af9c
aaliya-92 <aaliyakhurshid0791@gmail.com>
2024-09-04 02:04:42 +0530

    commit

---
+++ Question_4.py
@@ -0,0 +1,9 @@
+# Write a program in Python to compute simple Interest.
+
+p = int(input("Enter the principal amount:"))
+r = float(input("Enter the rate of interest:"))
+t = int(input("Enter the no of years:"))
+si = (p * r * t)/100
+print("The simple interest comes out to be: ₹",si)
+amount = p + si
+print("The total amount to be returned is: ₹",amount)
